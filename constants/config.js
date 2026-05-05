9eb35feac2ecc0aba6db1f3687c60bac1a102ebc
Ismael Ribeiro de Morais Netto <nettoetec@gmail.com>
2026-05-05 14:01:15 +0000

    Final aula 05

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
 // Ajuste este valor para o backend que está rodando no seu ambiente
 // Em geral em development: "http://localhost:3000" ou "http://10.0.2.2:3000" (Android emulator)
-export const API_URL = "process.env.EXPO_PUBLIC_API_URL";
-export const API_KEY = "process.env.EXPO_PUBLIC_API_KEY";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
